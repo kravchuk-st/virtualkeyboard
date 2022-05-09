104bea050189f8233b4817288609fff3cfb560f4
kravchuk-st <qkp@ukr.net>
2022-05-09 17:18:03 +0300

    feat: add languages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,396 @@
-const body = document.querySelector('body');
+import { keys } from './key.js';
 
-let main = document.createElement('main');
-    body.appendChild(main);
+window.addEventListener('keydown', (ev) => {
+  ev.preventDefault();
+})
+
+window.addEventListener('keyup', (ev) => {
+  ev.preventDefault();
+})
+
+let body = document.querySelector('body');
+body.addEventListener('keydown', (ev) => {
+  ev.preventDefault();
+})
+
+body.addEventListener('keyup', (ev) => {
+  ev.preventDefault();
+})
+
+let container = document.createElement('div');
+container.classList.add('container');
+body.prepend(container);
+
+let title = document.createElement('h1')
+title.innerText = 'RSS Виртуальная клавиатура';
+container.append(title);
+
+let textarea = document.createElement('textarea');
+textarea.classList.add('body-textarea');
+textarea.id = 'textarea';
+textarea.setAttribute('cols', '70');
+textarea.setAttribute('rows', '10');
+container.append(textarea);
+
+let keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+keyboard.id = 'keyboard';
+container.append(keyboard);
+
+let description = document.createElement('p');
+description.classList.add('description');
+description.innerText = 'Клавиатура создана в операционной системе Windows';
+container.append(description);
+
+let language = document.createElement('p');
+language.classList.add('language');
+language.innerText = 'Для переключения языка используйте комбинацию клавиш: левыe Ctrl + Shift';
+container.append(language);
+
+
+
+
+class Keyboard {
+    
+    langOptions = {
+        'en': 'en',
+        'ru': 'ru',
+    }
+
+    // lang = this.langOptions.en;
+
+    
+    value = "";
+
+    keyCodes = [...keys];
+
+    specialKeyCodes = [
+        {'Enter': `\r\n`},
+        {'Tab': '    '},
+    ];
+
+    isShiftACtive = false;
+    isCapsLockActive = false;
+
+
+    constructor() {
+        this.lang = this.getSetingds();
+        this.createVirtualKeyboard();
+
+        this.lisenKeybord();
+        this.lisenScreen();
+        this.saveSetingds();
+    }
+    
+
+    saveSetingds() {
+      if(window.localStorage) {
+        document.onkeydown = function(ev) {
+          if(ev.keyCode === 17) {
+            document.onkeyup = function(ev) {
+              if (ev.keyCode === 16) {
+                if (localStorage.getItem("lang") === 'en') {
+                  localStorage.setItem("lang", "ru");
+                } else {
+                  localStorage.setItem("lang", "en");
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+    getSetingds() {
+      if(window.localStorage) {
+        if (localStorage.getItem("lang")) {
+          if (localStorage.getItem("lang") === 'en') {
+            return this.langOptions.en
+          } else if (localStorage.getItem("lang") === 'ru') {
+            return this.langOptions.ru
+          }
+        } else {
+          return this.langOptions.en
+        }
+      }
+    }
+
+    findSpecialSymbol(eventKey) {
+      let res;
+        this.specialKeyCodes.forEach((obj) => {
+          const keySymbolArr = Object.entries(obj);
+          if (eventKey === keySymbolArr[0][0]) {
+              res = keySymbolArr[0][1];
+              return;
+          }
+      })
+
+      return res;
+    }
+
+    lisenKeybord() {
+        document.getElementById('textarea').addEventListener('keypress', ev => ev.preventDefault());
+
+        window.addEventListener('keydown', (ev) => {
+          let el = document.querySelector(`div[data-keycode='${ev.keyCode}']`);
+          if (ev.keyCode !== 20 && ev.keyCode !== 16 && ev.keyCode !== 17 && ev.keyCode !== 18) {
+            el.classList.add('active');
+            setTimeout(function(){
+              el.classList.remove('active');
+            }, 300)
+          }
+          let ind;
+          keys.forEach((el, index) => {
+            if (Object.values(el).includes(+ev.keyCode)) {
+              ind = index;
+            }
+          });
+
+          const textSymbol = this.findSpecialSymbol(ev.key);
+
+          switch (this.lang) {
+            case this.langOptions.en:
+              if ((this.isShiftACtive === true && this.isCapsLockActive === false) || (this.isShiftACtive === false && this.isCapsLockActive === true)) {
+                textarea.value += (textSymbol) ? textSymbol : (keys[ind]['en']).toUpperCase();
+              } else{
+                textarea.value += (textSymbol) ? textSymbol : keys[ind]['en'].toLowerCase();
+              }
+
+              break;
+
+            case this.langOptions.ru:
+              if ((this.isShiftACtive === true && this.isCapsLockActive === false) || (this.isShiftACtive === false && this.isCapsLockActive === true)) {
+                textarea.value += (textSymbol) ? textSymbol : (keys[ind]['ru']).toUpperCase();
+              } else{
+                textarea.value += (textSymbol) ? textSymbol : keys[ind]['ru'].toLowerCase();
+              }
+              break;
+
+            default:
+              break;
+          }
+
+          
+          });
+    }
+
+
+    lisenScreen() {
+        document.querySelectorAll('.key').forEach((el) => {
+            el.addEventListener('click', (ev) => {
+              if ((ev.target.dataset.keycode !== '20') && (ev.target.dataset.keycode !== '16') && (ev.target.dataset.keycode !== '17') && (ev.target.dataset.keycode !== '18')) {
+                let el = document.querySelector(`div[data-keycode='${ev.target.dataset.keycode}']`);
+                el.classList.add('active');
+                let ind;
+                keys.forEach((el, index) => {
+                  if (Object.values(el).includes(+ev.target.dataset.keycode)) {
+                    ind = index;
+                  }
+                });
+                textarea.value += keys[ind][this.lang];
+    
+                setTimeout(function(){
+                  el.classList.remove('active');
+                }, 200)
+              }
+            });
+        })
+    }
+
+
+    createVirtualKeyboard() {
+        // add html and events
+        let out = '';
+        switch (this.lang) {
+            case this.langOptions.en:
+              this.keyCodes.forEach(el => {
+                out += `<div class="key" data-keyCode="${el.keyCode}">${el.en}</div>`;
+
+
+                // keyElement.addEventListener("click", () => {
+                //     this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                //     this._triggerEvent("oninput");
+                // });
+              })
+                
+              break;
+
+            case this.langOptions.ru:
+            
+                this.keyCodes.forEach(el => {
+                    out += `<div class="key" data-keyCode="${el.keyCode}">${el.ru}</div>`;
+
+
+                    // keyElement.addEventListener("click", () => {
+                    //     this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                    //     this._triggerEvent("oninput");
+                    // });
+                })
+                
+                break;
+        
+            default:
+                break;
+
+        }
+        document.querySelector('.keyboard').innerHTML = out;
+
+        const backspace = document.querySelector("div[data-keycode='8']");
+        backspace.classList.add('Backspace');
+        backspace.innerText = 'Backspace';
+
+        const tab = document.querySelector("div[data-keycode='9']");
+        tab.classList.add('Tab');
+        tab.innerText = 'Tab';
+
+        const enter = document.querySelector("div[data-keycode='13']");
+        enter.classList.add('Enter');
+        enter.innerText = 'Enter';
+        // enter.key = '\n';
+
+        const shiftLeft = document.querySelectorAll("div[data-keycode='16']")[0];
+        shiftLeft.classList.add('ShiftLeft');
+        shiftLeft.innerText = 'Shift';
+
+        const shiftRight = document.querySelectorAll("div[data-keycode='16']")[1];
+        shiftRight.classList.add('ShiftRight');
+        shiftRight.innerText = 'Shift';
+
+        const ctrlLeft = document.querySelectorAll("div[data-keycode='17']")[0];
+        ctrlLeft.classList.add('ControlLeft');
+        ctrlLeft.innerText = 'Ctrl';
+
+        const ctrlRight = document.querySelectorAll("div[data-keycode='17']")[1];
+        ctrlRight.classList.add('ControlRight');
+        ctrlRight.innerText = 'Ctrl';
+
+        const altLeft = document.querySelectorAll("div[data-keycode='18']")[0];
+        altLeft.classList.add('AltLeft');
+        altLeft.innerText = 'Alt';
+
+        const altRight = document.querySelectorAll("div[data-keycode='18']")[1];
+        altRight.classList.add('AltRight');
+        altRight.innerText = 'Alt';
+
+        const win = document.querySelector("div[data-keycode='91']");
+        win.classList.add('Win');
+        win.innerText = 'Win';
+
+        const capsLock = document.querySelector("div[data-keycode='20']");
+        capsLock.classList.add('CapsLock');
+        capsLock.innerText = 'CapsLock';
+
+        const arrowUp = document.querySelector("div[data-keycode='38']");
+        arrowUp.innerText = '\u2191';
+
+        const arrowLeft = document.querySelector("div[data-keycode='37']");
+        arrowLeft.innerText = '\u2190';
+
+        const arrowDown = document.querySelector("div[data-keycode='40']");
+        arrowDown.innerText = '\u2193';
+
+        const arrowRight = document.querySelector("div[data-keycode='39']");
+        arrowRight.innerText = '\u2192';
+
+        const space = document.querySelector("div[data-keycode='32']");
+        space.classList.add('Space');
+
+        const del = document.querySelector("div[data-keycode='46']");
+        del.classList.add('Del');
+        del.innerText = 'del';
+    }
+
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  const keybord = new Keyboard();
+
+  let isShift = false;
+  let isCLock = false;
+
+  let capsLockKey = document.querySelector('.CapsLock');
+  capsLockKey.addEventListener('click', (ev) => {
+    capsLockKey.classList.toggle('active');
+  });
+
+  document.addEventListener('click', (ev) => {
+    if (ev.target.classList.contains('ControlLeft')) {
+      ev.target.classList.add('active');
+      setTimeout(function(){
+        ev.target.classList.remove('active');
+      }, 300)
+    }
+
+    if (ev.target.classList.contains('ControlRight')) {
+      ev.target.classList.add('active');
+      setTimeout(function(){
+        ev.target.classList.remove('active');
+      }, 300)
+    }
+
+    if (ev.target.classList.contains('AltLeft')) {
+      ev.target.classList.add('active');
+      setTimeout(function(){
+        ev.target.classList.remove('active');
+      }, 300)
+    }
+
+    if (ev.target.classList.contains('AltRight')) {
+      ev.target.classList.add('active');
+      setTimeout(function(){
+        ev.target.classList.remove('active');
+      }, 300)
+    }
+  });
+
+  window.addEventListener('keydown', (ev) => {
+    if (ev.keyCode === 20) {
+      ev.preventDefault();
+      capsLockKey.classList.toggle('active');
+    }
+
+    if (ev.keyCode === 17) {
+      document.querySelector(`.${ev.code}`).classList.add('active');
+      setTimeout(function(){
+        document.querySelector(`.${ev.code}`).classList.remove('active');
+      }, 300)
+    }
+
+    if (ev.keyCode === 18) {
+      document.querySelector(`.${ev.code}`).classList.add('active');
+      setTimeout(function(){
+        document.querySelector(`.${ev.code}`).classList.remove('active');
+      }, 300)
+    }
+  })
+
+});
+
+// document.onkeydown = function(ev) {
+//   if(ev.keyCode === 17) {
+//     document.onkeyup = function(ev) {
+//       if (ev.keyCode === 16) {
+//         document.querySelector('.keyboard').innerHTML = '';
+//         Keyboard.createVirtualKeyboard();
+//       }
+//     }
+//   }
+// }
+
+// function changeCaseText() {
+//   let capsLock = document.querySelector(".CapsLock");
+//   capsLock.addEventListener('click', () => {
+//     this.isCapsLockActive = !this.isCapsLockActive;
+//     capsLock.classList.toggle('active');
+//   })
+
+//   window.addEventListener('keydown', (ev) => {
+//     if (ev.keyCode === 20) {
+//       ev.preventDefault();
+//       this.isCapsLockActive = !this.isCapsLockActive;
+//       capsLock.classList.toggle('active');
+//     }
+//   })
+// }
---
+++ js/key.js
@@ -0,0 +1,684 @@
+export const keys = [
+  {
+      "en": "`",
+      "ru": "ё",
+      "shift": "~",
+      "keyCode": 192,
+      "which": 192,
+      "code": "Backquote",
+      "description": "Backtick / grave accent / ñ / æ / ö"
+  },
+  {
+      "en": "1",
+      "ru": "1",
+      "shift": "!",
+      "keyCode": 49,
+      "which": 49,
+      "code": "Digit1",
+      "description": "1 Key",
+      "unicode": "①"
+  },
+  {
+      "en": "2",
+      "ru": "2",
+      "shift": "@",
+      "keyCode": 50,
+      "which": 50,
+      "code": "Digit2",
+      
+      "description": "2 Key",
+      "unicode": "②"
+  },
+  {
+      "en": "3",
+      "ru": "3",
+      "shift": "#",
+      "keyCode": 51,
+      "which": 51,
+      "code": "Digit3",
+      
+      "description": "3 Key",
+      "unicode": "③"
+  },
+  {
+      "en": "4",
+      "ru": "4",
+      "shift": "$",
+      "keyCode": 52,
+      "which": 52,
+      "code": "Digit4",
+      
+      "description": "4 Key",
+      "unicode": "④"
+  },
+  {
+      "en": "5",
+      "ru": "5",
+      "shift": "%",
+      "keyCode": 53,
+      "which": 53,
+      "code": "Digit5",
+      
+      "description": "5 Key",
+      "unicode": "⑤"
+  },
+  {
+      "en": "6",
+      "ru": "6",
+      "shift": "^",
+      "keyCode": 54,
+      "which": 54,
+      "code": "Digit6",
+      
+      "description": "6 Key",
+      "unicode": "⑥"
+  },
+  {
+      "en": "7",
+      "ru": "7",
+      "shift": "&",
+      "keyCode": 55,
+      "which": 55,
+      "code": "Digit7",
+      
+      "description": "7 Key",
+      "unicode": "⑦"
+  },
+  {
+      "en": "8",
+      "ru": "8",
+      "shift": "*",
+      "keyCode": 56,
+      "which": 56,
+      "code": "Digit8",
+      
+      "description": "8 Key",
+      "unicode": "⑧"
+  },
+  {
+      "en": "9",
+      "ru": "9",
+      "shift": "(",
+      "keyCode": 57,
+      "which": 57,
+      "code": "Digit9",
+      
+      "description": "9 Key",
+      "unicode": "⑨"
+  },
+  {
+      "en": "0",
+      "ru": "0",
+      "shift": ")",
+      "keyCode": 48,
+      "which": 48,
+      "code": "Digit0",
+      
+      "description": "0",
+      "unicode": "⓪"
+  },
+  {
+      "en": "-",
+      "ru": "-",
+      "shift": "_",
+      "keyCode": 189,
+      "which": 189,
+      "code": "NumpadSubtract",
+      "location": 3,
+      "description": "subtract"
+  },
+  {
+      "en": "=",
+      "ru": "=",
+      "shift": "+",
+      "keyCode": 187,
+      "which": 187,
+      "code": "Equal",
+      
+      "description": "equals (firefox)"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 8,
+      "which": 8,
+      "code": "Backspace",
+      
+      "description": "backspace / delete",
+      "unicode": "⌫"
+  },
+  {
+      "en": "    ",
+      "ru": "    ",
+      "keyCode": 9,
+      "which": 9,
+      "code": "Tab",
+      
+      "description": "tab",
+      "unicode": "↹"
+  },
+  {
+      "en": "q",
+      "ru": "й",
+      "keyCode": 81,
+      "which": 81,
+      "code": "KeyQ",
+      
+      "description": "q"
+  },
+  {
+      "en": "w",
+      "ru": "ц",
+      "keyCode": 87,
+      "which": 87,
+      "code": "KeyW",
+      
+      "description": "w"
+  },
+  {
+      "en": "e",
+      "ru": "у",
+      "keyCode": 69,
+      "which": 69,
+      "code": "KeyE",
+      
+      "description": "e"
+  },
+  {
+      "en": "r",
+      "ru": "к",
+      "keyCode": 82,
+      "which": 82,
+      "code": "KeyR",
+      
+      "description": "r"
+  },
+  {
+      "en": "t",
+      "ru": "е",
+      "keyCode": 84,
+      "which": 84,
+      "code": "KeyT",
+      
+      "description": "t"
+  },
+  {
+      "en": "y",
+      "ru": "н",
+      "keyCode": 89,
+      "which": 89,
+      "code": "KeyY",
+      
+      "description": "y"
+  },
+  {
+      "en": "u",
+      "ru": "г",
+      "keyCode": 85,
+      "which": 85,
+      "code": "KeyU",
+      
+      "description": "u"
+  },
+  {
+      "en": "i",
+      "ru": "ш",
+      "keyCode": 73,
+      "which": 73,
+      "code": "KeyI",
+      
+      "description": "i"
+  },
+  {
+      "en": "o",
+      "ru": "щ",
+      "keyCode": 79,
+      "which": 79,
+      "code": "KeyO",
+      
+      "description": "o"
+  },
+  {
+      "en": "p",
+      "ru": "з",
+      "keyCode": 80,
+      "which": 80,
+      "code": "KeyP",
+      
+      "description": "p"
+  },
+  {
+      "en": "[",
+      "ru": "х",
+      "shift": "{",
+      "keyCode": 219,
+      "which": 219,
+      "code": "BracketLeft",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "description": "^"
+  },
+  {
+      "en": "]",
+      "ru": "ъ",
+      "shift": "}",
+      "keyCode": 221,
+      "which": 221,
+      "code": "BracketRight",
+      
+      "description": "close bracket / å"
+  },
+  {
+      "en": "\\",
+      "ru": "\\",
+      "shift": "|",
+      "keyCode": 220,
+      "which": 220,
+      "code": "Backslash",
+      
+      "description": "back slash"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 46,
+      "which": 46,
+      // "code": "NumpadDecimal",
+      "code": "Delete",
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "description": "delete",
+      "unicode": "⌦"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 20,
+      "which": 20,
+      "code": "CapsLock",
+      
+      "description": "caps lock",
+      "unicode": "⇪"
+  },
+  {
+      "en": "a",
+      "ru": "ф",
+      "keyCode": 65,
+      "which": 65,
+      "code": "KeyA",
+      
+      "description": "a"
+  },
+  {
+      "en": "s",
+      "ru": "ы",
+      "keyCode": 83,
+      "which": 83,
+      "code": "KeyS",
+      
+      "description": "s"
+  },
+  {
+      "en": "d",
+      "ru": "в",
+      "keyCode": 68,
+      "which": 68,
+      "code": "KeyD",
+      
+      "description": "d"
+  },
+  {
+      "en": "f",
+      "ru": "а",
+      "keyCode": 70,
+      "which": 70,
+      "code": "KeyF",
+      
+      "description": "f"
+  },
+  {
+      "en": "g",
+      "ru": "п",
+      "keyCode": 71,
+      "which": 71,
+      "code": "KeyG",
+      
+      "description": "g"
+  },
+  {
+      "en": "h",
+      "ru": "р",
+      "keyCode": 72,
+      "which": 72,
+      "code": "KeyH",
+      
+      "description": "h"
+  },
+  {
+      "en": "j",
+      "ru": "о",
+      "keyCode": 74,
+      "which": 74,
+      "code": "KeyJ",
+      
+      "description": "j"
+  },
+  {
+      "en": "k",
+      "ru": "л",
+      "keyCode": 75,
+      "which": 75,
+      "code": "KeyK",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": false,
+      "description": "k"
+  },
+  {
+      "en": "l",
+      "ru": "д",
+      "keyCode": 76,
+      "which": 76,
+      "code": "KeyL",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": false,
+      "description": "l"
+  },
+  {
+      "en": ";",
+      "ru": "ж",
+      "shift": ":",
+      "keyCode": 186,
+      "which": 186,
+      "code": "Semicolon",
+      
+      "description": "semicolon (firefox), equals"
+  },
+  {
+      "en": "'",
+      "ru": "э",
+      "shift": "\"",
+      "keyCode": 222,
+      "which": 222,
+      "code": "Quote",
+      
+      "description": "single quote / ø / ä"
+  },
+  {
+      "en": "\n",
+      "ru": "\n",
+      "keyCode": 13,
+      "which": 13,
+      "code": "Enter",
+      
+      "description": "Enter / Return",
+      "unicode": "↵"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 16,
+      "which": 16,
+      "code": "ShiftLeft",
+      
+      "description": "shift",
+      "unicode": "⇧"
+  },
+  {
+      "en": "z",
+      "ru": "я",
+      "keyCode": 90,
+      "which": 90,
+      "code": "KeyZ",
+      
+      "description": "z"
+  },
+  {
+      "en": "x",
+      "ru": "ч",
+      "keyCode": 88,
+      "which": 88,
+      "code": "KeyX",
+      
+      "description": "x"
+  },
+  {
+      "en": "c",
+      "ru": "с",
+      "keyCode": 67,
+      "which": 67,
+      "code": "KeyC",
+      
+      "description": "c"
+  },
+  {
+      "en": "v",
+      "ru": "м",
+      "keyCode": 86,
+      "which": 86,
+      "code": "KeyV",
+      
+      "description": "v"
+  },
+  {
+      "en": "b",
+      "ru": "и",
+      "keyCode": 66,
+      "which": 66,
+      "code": "KeyB",
+      
+      "description": "b"
+  },
+  {
+      "en": "n",
+      "ru": "т",
+      "keyCode": 78,
+      "which": 78,
+      "code": "KeyN",
+      
+      "description": "n"
+  },
+  {
+      "en": "m",
+      "ru": "ь",
+      "keyCode": 77,
+      "which": 77,
+      "code": "KeyM",
+      
+      "description": "m"
+  },
+  {
+      "en": ",",
+      "ru": "б",
+      "shift": "<",
+      "keyCode": 188,
+      "which": 188,
+      "code": "NumpadDecimal",
+      "location": 3,
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "description": "numpad period (firefox)"
+  },
+  {
+      "en": ".",
+      "ru": "ю",
+      "shift": ">",
+      "keyCode": 190,
+      "which": 190,
+      "code": "Period",
+      
+      "description": "period"
+  },
+  {
+      "en": "/",
+      "ru": ".",
+      "shift": "?",
+      "keyCode": 191,
+      "which": 191,
+      "code": "Slash",
+      
+      "description": "forward slash / ç"
+  },
+  {
+      "en": "\u2191",
+      "ru": "\u2191",
+      "keyCode": 38,
+      "which": 38,
+      "code": "ArrowUp",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "repeat": false,
+      "description": "ArrowUp",
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 16,
+      "which": 16,
+      "code": "ShiftRight",
+      
+      "description": "Rshift",
+      "unicode": "⇧"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 17,
+      "which": 17,
+      "code": "ControlLeft",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": true,
+      "description": "Control",
+      "unicode": "^"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 91,
+      "which": 91,
+      "code": "MetaLeft",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": false,
+      "description": "Windows",
+      "unicode": "⌘ ⊞"
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 18,
+      "which": 18,
+      "code": "AltLeft",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": false,
+      "description": "Alt",
+      "unicode": "⎇ / ⌥"
+  },
+  {
+      "en": " ",
+      "ru": " ",
+      "keyCode": 32,
+      "which": 32,
+      "code": "Space",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "repeat": false,
+      "description": "Space",
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 18,
+      "which": 18,
+      "code": "AltRight",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": false,
+      "description": "Ralt",
+      "unicode": "⎇ / ⌥"
+  },
+  {
+      "en": "\u2190",
+      "ru": "\u2190",
+      "keyCode": 37,
+      "which": 37,
+      "code": "ArrowLeft",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "repeat": false,
+      "description": "ArrowLeft",
+  },
+  {
+      "en": "\u2193",
+      "ru": "\u2193",
+      "keyCode": 40,
+      "which": 40,
+      "code": "ArrowDown",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "repeat": false,
+      "description": "ArrowDown",
+  },
+  {
+      "en": "\u2192",
+      "ru": "\u2192",
+      "keyCode": 39,
+      "which": 39,
+      "code": "ArrowRight",
+      
+      "altKey": false,
+      "ctrlKey": false,
+      "metaKey": false,
+      "shiftKey": false,
+      "repeat": false,
+      "description": "ArrowRight",
+  },
+  {
+      "en": "",
+      "ru": "",
+      "keyCode": 17,
+      "which": 17,
+      "code": "ControlRight",
+      
+      "altKey": true,
+      "ctrlKey": false,
+      "metaKey": true,
+      "shiftKey": true,
+      "description": "Rctrl",
+      "unicode": "^"
+  },
+];
